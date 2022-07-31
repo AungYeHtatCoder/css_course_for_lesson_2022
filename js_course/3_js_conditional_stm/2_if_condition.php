e55cc3e0635f6c970d63d1df5e932f300b469f9f
AungYeHtatCoder <aungyehatacoder2021@gmail.com>
2022-07-31 21:50:01 +0630

    new_code_add_for_class

---
+++ js_course/3_js_conditional_stm/2_if_condition.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>js if condition</title>
+</head>
+
+<body>
+ <h1>js if condition </h1>
+ <script>
+ month = "July";
+ if (month == "July") {
+  document.write("It's summer");
+ } else {
+  document.write("It's not summer");
+ }
+ </script>
+</body>
+
+</html>
